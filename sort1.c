4a0debd9d11e549b737719f3c850a5bd537ffbc0
maarselo <marce@decaos.com>
2025-04-02 22:09:27 +0200

    Funciona mal

--- sort1.c
+++ sort1.c
@@ -22,8 +22,8 @@ int   ft_abs(int num)
 int ft_check_size(t_stack *stack)
 {
     if (stack->size % 2 != 0)
-        return (stack->size + 1);
-    return (stack->size);
+        return ((stack->size / 2) +1);
+    return (stack->size / 2);
 }
 
 int ft_calculate_costa(t_stack *stack_a, t_node *node)
@@ -43,32 +43,36 @@ int ft_calculate_costb(t_stack *stack_b, t_node *node) //cambiar el orden
     t_node  *min;
     t_node  *tmp;
 
-    cost_b = 1; //To push to b
+    cost_b = 0; //To push to b
     max = ft_max(stack_b);
     min = ft_min(stack_b);
     if ((node->index > max->index) || (node->index < min->index))
     {
         if (stack_b->top == max)
             return (cost_b);
-        if ((ft_getpos(max, stack_b) - 1) > (stack_b->size / 2))
-            cost_b = cost_b + (stack_b->size - (ft_getpos(max, stack_b) - 1));
+        if (ft_getpos(max, stack_b) > ft_check_size(stack_b))
+            cost_b = stack_b->size - ft_getpos(max, stack_b) + 1;
+        else
+            cost_b = ft_getpos(max, stack_b) - 1;
     }
    else
     {
         tmp = stack_b->top;
-        min = tmp; //like min prev
+        min = NULL; //like min prev
         while (tmp)
         {
-            if ((tmp->value < node->value) && (ft_abs(tmp->index - node->index) < ft_abs(min->index - node->index)))
+            if ((tmp->value < node->value) && (!min || ft_abs(tmp->index - node->index) < ft_abs(min->index - node->index)))
                 min = tmp;
             tmp = tmp->next;
         }
-        printf("nigga:%d", min->value);
-        cost_b = ft_getpos(min,stack_b);////////////////
-        if (cost_b > ft_check_size(stack_b))
-            cost_b = stack_b->size - cost_b + 1;
-        else
-            cost_b -= 1;
+        //printf("menor:%d", min->value);
+        if (ft_getpos(min,stack_b) > ft_check_size(stack_b)) {
+            cost_b = stack_b->size - ft_getpos(min,stack_b) + 1;
+            //printf("youuu");
+        }
+        else {
+            cost_b = ft_getpos(min,stack_b) - 1;
+        }
     }
     return (cost_b);
 }
@@ -81,11 +85,11 @@ t_node *ft_find_cheapest_node(t_stack *stack_a, t_stack *stack_b )
     int     cost;
 
     current = stack_a->top;
-    cheapest = current;
-    min_cost = ft_calculate_costa(stack_a, current) + ft_calculate_costb(stack_b, current);
+    cheapest = NULL;
+    min_cost = INT_MAX;
     while(current)
     {
-        cost = ft_calculate_costa(stack_a, current) + ft_calculate_costb(stack_b, current);
+        cost = ft_calculate_costa(stack_a, current) + ft_calculate_costb(stack_b, current) + 1;
         if (cost < min_cost)
         {
             min_cost = cost;
@@ -106,13 +110,13 @@ void    ft_push_to_stackb(t_stack *stack_a, t_stack *stack_b, t_node *cheapest)
 
     while (cost_a > 0 && cost_b > 0)
     {
-        if ((ft_getpos(cheapest, stack_a) <= (ft_check_size(stack_a) / 2)) && (cost_b <= (ft_check_size(stack_b) / 2)))
+        if ((ft_getpos(cheapest, stack_a) <= ft_check_size(stack_a)) && (cost_b <= ft_check_size(stack_b)))
         {
             ft_rr(stack_a, stack_b);
             cost_a--;
             cost_b--;
         }
-        else if ((ft_getpos(cheapest, stack_a) > (ft_check_size(stack_a) / 2)) && (cost_b > (ft_check_size(stack_b) / 2)))
+        else if ((ft_getpos(cheapest, stack_a) > ft_check_size(stack_a)) && (cost_b > ft_check_size(stack_b)))
         {
             ft_rrr(stack_a, stack_b);
             cost_a--;
@@ -123,7 +127,7 @@ void    ft_push_to_stackb(t_stack *stack_a, t_stack *stack_b, t_node *cheapest)
     }
     while (cost_a > 0)
     {
-        if (ft_getpos(cheapest, stack_a) <= (ft_check_size(stack_a) / 2))
+        if (ft_getpos(cheapest, stack_a) <= ft_check_size(stack_a))
             ft_rotate(stack_a, 'a');
         else
             ft_reverse(stack_a, 'a');
@@ -131,7 +135,7 @@ void    ft_push_to_stackb(t_stack *stack_a, t_stack *stack_b, t_node *cheapest)
     }
     while (cost_b > 0)
     {
-        if (cost_b <= (ft_check_size(stack_b) / 2))
+        if (cost_b <= ft_check_size(stack_b))
             ft_rotate(stack_b, 'b');
         else
             ft_reverse(stack_b, 'b');
@@ -140,22 +144,34 @@ void    ft_push_to_stackb(t_stack *stack_a, t_stack *stack_b, t_node *cheapest)
     ft_push(stack_a, stack_b, 'b');
 }
 
-void    ft_rotate_b_to_push(t_stack *stack_b)
+void    ft_rotate_b_to_pusha_a(t_stack *stack_b)
 {
     t_node *max;
 
     max = ft_max(stack_b);
     while (stack_b->top != max)
     {
-        if (ft_getpos(max, stack_b) <= (ft_check_size(stack_b) / 2))
+        if (ft_getpos(max, stack_b) <= ft_check_size(stack_b))
             ft_rotate(stack_b, 'b');
         else
             ft_reverse(stack_b, 'b');
     }
 }
 
+void    ft_printfvalue(t_stack *stack_a, t_stack *stack_b)
+{
+    t_node *tmp = stack_a->top;
+    while(tmp)
+    {
+        printf("Numer: %d Coste a: %d Costeb: %d + 1\n", tmp->value, ft_calculate_costa(stack_a, tmp), ft_calculate_costb(stack_b, tmp));
+        ft_print_stacks(stack_b);
+        //ft_push(stack_a, stack_b, '\0');
+        tmp = tmp->next;
+    }
+}
+
 void    ft_sorted_more(t_stack *stack_a, t_stack *stack_b)
-{/*
+{
     t_node  *cheapest;
 
     ft_push(stack_a, stack_b, 'b');
@@ -165,26 +181,13 @@ void    ft_sorted_more(t_stack *stack_a, t_stack *stack_b)
         cheapest = ft_find_cheapest_node(stack_a, stack_b);
         ft_push_to_stackb(stack_a, stack_b, cheapest);
     }
-    printf("\n\n");
     ft_print_stacks(stack_b);
-    printf("\n\n");
-    ft_rotate_b_to_push(stack_b);
-    printf("\n\n");
-    ft_print_stacks(stack_b);
-    printf("\n\n");
-*/
+/*
     ft_push(stack_a, stack_b, 'b');
     ft_push(stack_a, stack_b, 'b');
-    printf("\nmarcelo: %d\n", ft_calculate_costb(stack_b, stack_a->top));
-    ft_print_stacks(stack_b);
-    ft_push(stack_a, stack_b, 'b');
-    ft_sorted_three(stack_b);
-    printf("\nmarcelo: %d\n", ft_calculate_costb(stack_b, stack_a->top));
-    ft_print_stacks(stack_b); t_stack *stack_c = (t_stack*)malloc(sizeof(t_stack));
-    ft_push(stack_a, stack_b, 'b');
-    ft_sorted_four(stack_b, stack_c);
-    printf("\nmarcelo: %d\n", ft_calculate_costb(stack_b, stack_a->top));
-    ft_print_stacks(stack_b);
+    ft_printfvalue(stack_a, stack_b);
+    //ft_print_stacks(stack_b)
+;*/
 }
 
 /*
