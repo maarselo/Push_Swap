06884ee81bd7aba25ddeccae8ec51e88af8affd3
maarselo <marce@decaos.com>
2025-04-05 18:45:22 +0200

    Add final entry.c without INT MAX corrections

--- entry.c
+++ entry.c
@@ -6,72 +6,72 @@
 /*   By: mvillavi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 15:14:01 by mvillavi          #+#    #+#             */
-/*   Updated: 2025/03/16 15:14:02 by mvillavi         ###   ########.fr       */
+/*   Updated: 2025/04/05 18:44:41 by mvillavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int ft_checkdup(char **argv)
+char	**ft_check_split(char **argv, int argc)
 {
-    int i;
-    int j;
+	char	**av;
 
-    i = 0;
-    while(argv[i])
-    {
-        j = i + 1;
-        while (argv[j])
-        {
-            if(!ft_strncmp(argv[i], argv[j], 11))
-                return (1);
-            j++;
-        }
-        i++;
-    }
-    return (0);
+	if (argc == 2)
+		av = ft_split(argv[1], ' ');
+	else
+		av = ++argv;
+	return (av);
 }
 
-static int   ft_checkdigit(char *c)
+static int	ft_checkdup(char **argv)
 {
-    if (!c)
-        return (1);
-    if (*c == '-' || *c == '+')
-        c++;
-    if (!*c)
-        return (1);
-    while (*c)
-    {
-        if (!ft_isdigit(*c))
-            return (1);
-        c++;
-    }
-    return (0);
-}
-//Comprueba que sean numeros los parametros y diferenfes
-int ft_check_args(char **argv)
-{
-    int i;
+	int	i;
+	int	j;
 
-    i = 1;
-    if (ft_checkdup(argv))
-        return (1);
-    while (argv[i]) {
-        if (ft_checkdigit(argv[i]))
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (argv[i])
+	{
+		j = i + 1;
+		while (argv[j])
+		{
+			if (!ft_strncmp(argv[i], argv[j], 11))
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
 
-//Comprueba si necesita hacer un split o no;
-char    **ft_check_split_argv(char **argv, int argc)
+static int	ft_checkdigit(char *c)
 {
-    char **av;
+	if (!c)
+		return (1);
+	if (*c == '-' || *c == '+')
+		c++;
+	if (!*c)
+		return (1);
+	while (*c)
+	{
+		if (!ft_isdigit(*c))
+			return (1);
+		c++;
+	}
+	return (0);
+}
 
-    if (argc == 2)
-        av = ft_split(argv[1], ' ');
-    else
-        av = ++argv;
-    return (av);
+int	ft_check_args(char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (ft_checkdup(argv))
+		return (1);
+	while (argv[i])
+	{
+		if (ft_checkdigit(argv[i]))
+			return (1);
+		i++;
+	}
+	return (0);
 }
