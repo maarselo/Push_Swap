b1f707e3808fed9318a4632a95086499c57737ee
maarselo <marce@decaos.com>
2025-04-12 13:30:33 +0200

    Add functions GNL for bonus

--- Libft/libft.h
+++ Libft/libft.h
@@ -72,4 +72,5 @@ t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
+char	*get_next_line(int fd);
 #endif
