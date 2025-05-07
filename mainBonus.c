58cf5dd0730a3dcc4cd8b497e4c73f89e12c486c
maarselo <marce@decaos.com>
2025-05-07 11:49:55 +0200

    Protect if receives a invlid move

--- mainBonus.c
+++ mainBonus.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mvillavi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/12 12:56:48 by mvillavi          #+#    #+#             */
-/*   Updated: 2025/04/12 12:56:49 by mvillavi         ###   ########.fr       */
+/*   Created: 2025/05/07 11:49:04 by mvillavi          #+#    #+#             */
+/*   Updated: 2025/05/07 11:49:06 by mvillavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,36 +26,45 @@ static void	ft_remove_n(char *line)
 	}
 }
 
-static void	ft_make_move(char *instruction, t_stack *stack_a, t_stack *stack_b)
+static int	ft_make_move1(char *instruction, t_stack *stack_a, t_stack *stack_b)
 {
 	size_t	len_instruction;
 
 	len_instruction = ft_strlen(instruction);
 	if (!ft_strncmp(instruction, "sa", len_instruction))
-		ft_swap_bonus(stack_a);
+		return (ft_swap_bonus(stack_a), 1);
 	else if (!ft_strncmp(instruction, "sb", len_instruction))
-		ft_swap_bonus(stack_b);
+		return (ft_swap_bonus(stack_b), 1);
 	else if (!ft_strncmp(instruction, "ss", len_instruction))
-		ft_ss_bonus(stack_a, stack_b);
+		return (ft_ss_bonus(stack_a, stack_b), 1);
 	else if (!ft_strncmp(instruction, "pa", len_instruction))
-		ft_push_bonus(stack_b, stack_a);
+		return (ft_push_bonus(stack_b, stack_a), 1);
 	else if (!ft_strncmp(instruction, "pb", len_instruction))
-		ft_push_bonus(stack_a, stack_b);
+		return (ft_push_bonus(stack_a, stack_b), 1);
 	else if (!ft_strncmp(instruction, "ra", len_instruction))
-		ft_rotate_bonus(stack_a);
-	else if (!ft_strncmp(instruction, "rb", len_instruction))
-		ft_rotate_bonus(stack_b);
-	else if (!ft_strncmp(instruction, "rr", len_instruction))
-		ft_rr_bonus(stack_a, stack_b);
-	else if (!ft_strncmp(instruction, "rra", len_instruction))
-		ft_reverse_bonus(stack_a);
-	else if (!ft_strncmp(instruction, "rrb", len_instruction))
-		ft_reverse_bonus(stack_b);
-	else if (!ft_strncmp(instruction, "rrr", len_instruction))
-		ft_rrr_bonus(stack_a, stack_b);
+		return (ft_rotate_bonus(stack_a), 1);
+	return (0);
 }
 
-static void	ft_sort_checker(t_stack *stack_a, t_stack *stack_b)
+static int	ft_make_move2(char *instruction, t_stack *stack_a, t_stack *stack_b)
+{
+	size_t	len_instruction;
+
+	len_instruction = ft_strlen(instruction);
+	if (!ft_strncmp(instruction, "rb", len_instruction))
+		return (ft_rotate_bonus(stack_b), 1);
+	else if (!ft_strncmp(instruction, "rr", len_instruction))
+		return (ft_rr_bonus(stack_a, stack_b), 1);
+	else if (!ft_strncmp(instruction, "rra", len_instruction))
+		return (ft_reverse_bonus(stack_a), 1);
+	else if (!ft_strncmp(instruction, "rrb", len_instruction))
+		return (ft_reverse_bonus(stack_b), 1);
+	else if (!ft_strncmp(instruction, "rrr", len_instruction))
+		return (ft_rrr_bonus(stack_a, stack_b), 1);
+	return (0);
+}
+
+static void	ft_sort_checker(t_stack *stk_a, t_stack *stk_b, int ac, char **av)
 {
 	char	*instruction;
 
@@ -63,7 +72,13 @@ static void	ft_sort_checker(t_stack *stack_a, t_stack *stack_b)
 	while (instruction)
 	{
 		ft_remove_n(instruction);
-		ft_make_move(instruction, stack_a, stack_b);
+		if (!ft_make_move1(instruction, stk_a, stk_b)
+			&& !ft_make_move2(instruction, stk_a, stk_b))
+		{
+			free (instruction);
+			ft_free(ac, av, stk_a, stk_b);
+			ft_error();
+		}
 		free(instruction);
 		instruction = get_next_line(0);
 		if (!instruction)
@@ -85,7 +100,7 @@ int	main(int argc, char **argv)
 	stack_a = ft_create_stack();
 	stack_b = ft_create_stack();
 	ft_fill_stack(stack_a, numbers);
-	ft_sort_checker(stack_a, stack_b);
+	ft_sort_checker(stack_a, stack_b, argc, argv);
 	if (!ft_check_sorted(stack_a) && stack_b->size == 0)
 		ft_putstr_fd("OK\n", 1);
 	else
